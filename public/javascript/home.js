52b8ee1050cfe40b6d9ebd45aebb216fb15b6b41
Sahana Balram <sahana2008balram@gmail.com>
2017-10-10 09:45:35 -0700

    Ajax call to the server

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -2,3 +2,17 @@ $(document).ready(function () {
     $('.slider').slider();
     $(".button-collapse").sideNav();
 });
+
+function displayScrape() {
+    $.getJSON("/all" ,function(data){
+        console.log(data);
+        $("#display-results").empty();
+        for (var i = 0; i < data.length; i++) {
+            var tr;
+            tr = $("<tr/>");
+            tr.append("<td>" + data[i].title + "</td>");
+            tr.append("<td>" + data[i].link + "</td>");
+            $("#display-results").first().append(tr);
+        }
+    });
+}
