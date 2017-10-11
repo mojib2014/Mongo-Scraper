c3c17ff3a4e7e29b207aa886d815b701fb900acd
Sahana Balram <sahana2008balram@gmail.com>
2017-10-10 22:13:24 -0700

    Start the code for note display

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,8 +1,3 @@
-$(document).ready(function () {
-    $('.slider').slider();
-    $(".button-collapse").sideNav();
-});
-
 function displayScrape() {
     $.getJSON("/all", function (data) {
         console.log(data);
@@ -17,17 +12,17 @@ function displayScrape() {
             var spanTitle = $("<span>");
             spanTitle.addClass("card-title");
             spanTitle.text(data[i].title);
-            var p = $("<p>");            
+            var p = $("<p>");
             cardContentDiv.append(spanTitle);
             cardContentDiv.append(p);
 
             var cardActionDiv = $("<div>");
             cardActionDiv.addClass("card-action");
             var a = $("<a>");
-            a.attr("href",data[i].link);
+            a.attr("href", data[i].link);
             a.text("Go to the article");
             cardActionDiv.append(a);
-            
+
             var button = $("<a>");
             button.addClass("waves-effect waves-light btn");
             button.text("Create Notes");
@@ -40,3 +35,21 @@ function displayScrape() {
         }
     });
 }
+
+$(document).on("click","a",function(){
+    $("#notes").empty();
+    var thisId = $(this).attr("");
+
+    $.ajax({
+        method: "GET",
+        url: "/articles" + thisId
+    })
+    .done(function(data){
+        $("#notes").append("<h2>"+ data.title+ "</h2>");
+        
+    });
+});
+$(document).ready(function () {
+    $('.slider').slider();
+    $(".button-collapse").sideNav();
+});
