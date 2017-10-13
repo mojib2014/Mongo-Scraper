3e885fe4a2925b13074a0ca34fc159e12e4bcde9
Sahana Balram <sahana2008balram@gmail.com>
2017-10-12 21:45:47 -0700

    Move code to saved.js

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,65 +1,14 @@
-function displaySaved() {
-    $.getJSON("/saved/all", function (data) {
-        console.log(data);
-        $("#nyt").empty();
-        $("#total-number").text(data.length);
-        for (var i = 0; i < data.length; i++) {
-            var mainDiv = $("<div>");
-            mainDiv.addClass("card blue-grey darken-1");
-            mainDiv.attr("id", "main-" + data[i]._id);
-            var cardContentDiv = $("<div>");
-            cardContentDiv.addClass("card-content white-text");
-            var spanTitle = $("<span>");
-            spanTitle.addClass("card-title");
-            spanTitle.attr("data-id", data[i]._id);
-            spanTitle.attr("id", "title-" + data[i]._id);
-            spanTitle.text(data[i].title);
-            var p = $("<p>");
-            p.text(data[i].summary);
-            p.attr("id", "summary-" + data[i]._id);
-            cardContentDiv.append(spanTitle);
-            cardContentDiv.append(p);
-            var cardActionDiv = $("<div>");
-            cardActionDiv.addClass("card-action");
-            var a = $("<a>");
-            a.attr("href", data[i].link);
-            a.attr("id", "link-" + data[i]._id);
-            a.text("Go to the article");
-            cardActionDiv.append(a);
-            var button = $("<a>");
-            button.addClass("waves-effect waves-light btn create-note modal-trigger");
-            button.attr("data-id", data[i]._id);
-            button.attr("data-target", "notes");
-            button.text("Create Notes");
-            var deleteArticle = $("<a>");
-            deleteArticle.addClass("waves-effect waves-light btn delete-button");
-            deleteArticle.attr("id", data[i]._id);
-            deleteArticle.text("Delete");
-            var byline = $("<p>");
-            byline.text(data[i].byline);
-            cardActionDiv.append(byline);
-            cardActionDiv.append(button);
-            cardActionDiv.append(deleteArticle);
-            mainDiv.append(cardContentDiv);
-            mainDiv.append(cardActionDiv);
-
-            $("#nyt").append(mainDiv);
-        }
-    });
-}
-
 function displayScrape() {
     $.getJSON("/articles", function (data) {
-        $("#nyt").empty();
+        $("#nyt-0").empty();
+        $("#nyt-1").empty();
+        $("#nyt-2").empty();
         $("#total-number").text(data.length);
         for (var i = 0; i < data.length; i++) {
             var mainDiv = $("<div>");
-
             mainDiv.addClass("card blue-grey darken-1");
-
             var cardContentDiv = $("<div>");
             cardContentDiv.addClass("card-content white-text");
-
             var spanTitle = $("<span>");
             spanTitle.addClass("card-title");
             spanTitle.attr("data-id", data[i]._id);
@@ -68,10 +17,8 @@ function displayScrape() {
             var p = $("<p>");
             p.text(data[i].summary);
             p.attr("id", "summary-" + data[i]._id);
-
             cardContentDiv.append(spanTitle);
             cardContentDiv.append(p);
-
             var cardActionDiv = $("<div>");
             cardActionDiv.addClass("card-action");
             var a = $("<a>");
@@ -91,100 +38,13 @@ function displayScrape() {
             cardActionDiv.append(saveArticle);
             mainDiv.append(cardContentDiv);
             mainDiv.append(cardActionDiv);
-            $("#nyt").append(mainDiv);
+            $("#nyt-" + String(i % 3)).append(mainDiv);
         }
     });
 }
 
-
 $(document).ready(function () {
     $('.slider').slider();
     $(".button-collapse").sideNav();
-    $('.modal').modal();
-
-    // When click on savearticle button
-    $(document).on('click', '.save-button', function () {
-        var thisId = $(this).attr("id");
-        var summary = $("#summary-" + thisId).text();
-        var title = $("#title-" + thisId).text();
-        var link = $("#link-" + thisId).attr('href');
-        var byline = $("#byline-" + thisId).text();
-        var data = {
-            "id": thisId,
-            "summary": summary,
-            "title": title,
-            "link": link,
-            "byline": byline
-        };
-        console.log(data);
-        $.ajax({
-            type: "POST",
-            url: "/save",
-            data: data,
-            dataType: "json",
-            success: function (data, textStatus) {
-                console.log(data);
-            }
-        });
-    });
-    // When click on delete article button
-    $(document).on('click', '.delete-button', function () {
-        var thisId = $(this).attr("id");
-        var summary = $("#summary-" + thisId).text();
-        var title = $("#title-" + thisId).text();
-        var link = $("#link-" + thisId).attr('href');
-        var byline = $("#byline-" + thisId).text();
-        var data = {
-            "_id": thisId
-        };
-        $.ajax({
-            type: "DELETE",
-            url: "/delete",
-            data: data,
-            success: function (data, textStatus) {
-                $("#main-" + thisId).remove();
-            }
-        })
-    });
-
-    // create note
-    $(document).on("click", ".create-note", function (data) {
-        // alert($(this).attr("data-id"));
-        $("#savenote").attr("data-id", $(this).attr("data-id"));
-        // $("#display-title").append($(this).attr("data-id"));
-        
-    });
-
-    // When you click the savenote button
-    $(document).on("click", "#savenote", function () {
-        // Grab the id associated with the article from the submit button
-       
-        // alert(thisId);
-        // get the user input value 
-        var thisId = $(this).attr("data-id");
-        var text =   $("#textarea1").val();
-        console.log(thisId);
-            // Run a POST request to change the note, using what's entered in the inputs
-        $.ajax({
-            type: "POST",
-            url: "/notes",
-            data: {
-                "_id" : thisId,
-                "body": text
-            },
-            success: function(data,textStatus,jqXHR) {
-
-            }
-
-        });
-    });
-    // delete note button
-    $(document).on("click", "#deletenote", function () {
-        // Grab the id associated with the article from the submit button
-        // var thisId = $(this).attr("data-id");
-        // alert(thisId);
-
-        // Run a DELETE request to change the note, using what's entered in the inputs
-
-    });
+    $('.modal').modal();   
 });
